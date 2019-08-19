b587a832ae11c2f17b20fd4a7ad91852cc739d29
cooksey <suitcase24@gmail.com>
2019-08-18 23:51:45 -0400

    Simplify job

--- src/taxi_trips/passenger_count.py
+++ src/taxi_trips/passenger_count.py
@@ -1,52 +1,62 @@
 import os
+import io
 import logging
 import csv
 import requests
-
-from taxi_trips import job_runner
 
 logger = logging.getLogger(__name__)
 
 YELLOW_TAXI_ENDPOINT = 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_{year}-{month}.csv'
 
-JOB_NAME = 'count_passengers'
-FILE_FORMAT = '{year}_{month}_{name}{checkpoint_handle}.csv'
-CHECKPOINT_HANDLE_FORMAT = '_stage={stage_counter}-{job_name}'
-RESULT_PATH = 'data/results/'
-CHECKPOINT_PATH = 'data/checkpoint/'
+FILE_FORMAT = '{year}_{month}_count_passengers.csv'
+RESULT_PATH = 'data/'
 
 
 def start_job(config=os.environ):
-    stages = [
-        job_runner.JobStage(job=pull_file, params={
-            'year': config['YEAR'],
-            'month': config['MONTH']
-        }),
-        job_runner.JobStage(job=count_people)
-    ]
-    job_runner.run_linear_stages(stages, config)
+    logger.info('Starting count_passengers job')
+    taxi_report = pull_file(year=config['YEAR'], month=config['MONTH'])
+    number_of_passengers = count_people(taxi_report)
+    save_csv_file(year=config['YEAR'], month=config['MONTH'], results=number_of_passengers)
+    logger.info('Finished count_passengers job')
 
 
 def pull_file(year, month):
-    url = YELLOW_TAXI_ENDPOINT.format(year, month)
+    url = YELLOW_TAXI_ENDPOINT.format(year=year, month=month)
 
     logger.info(f'Started pulling file from {url}')
     response = requests.get(url, allow_redirects=True)
     logger.info(f'Finished pulling file from {url} with status code: {response.status_code}')
 
-    return response.content
+    return io.StringIO(response.text)
 
 
-def count_people(previous_checkpoint):
-    logger.info('Starting count passenger stage. Reading checkpoint: {}'.format(previous_checkpoint))
-    with open(previous_checkpoint) as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',')
-        passenger_count = 0
-        for row in csv_reader:
-            try:
-                passenger_count += int(row['passenger_count'])
-            except ValueError:
-                logger.error('Failed to parse passenger_count on line: {}'.format(csv_reader.line_num))
-    logger.info('Finished count passenger stage')
+def count_people(taxi_report):
+    logger.info('Starting read csv report')
+    csv_reader = csv.DictReader(taxi_report)
+    passenger_count = 0
+    for row in csv_reader:
+        try:
+            passenger_count += int(row['passenger_count'])
+        except ValueError:
+            logger.error('Failed to parse passenger_count on line: {}'.format(csv_reader.line_num))
+    logger.info('Finished reading csv')
 
-    return str(passenger_count)
+    return passenger_count
+
+
+def save_csv_file(year, month, results):
+    file_key = FILE_FORMAT.format(year=year, month=month)
+    filename = '{path}{file_key}'.format(file_key=file_key, path=RESULT_PATH)
+
+    if not os.path.exists(os.path.dirname(RESULT_PATH)):
+        os.makedirs(os.path.dirname(RESULT_PATH), exist_ok=True)
+
+    with open(filename, 'w') as csv_file:
+        fieldnames = ['passenger_count']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({'passenger_count': results})
+
+    logger.info('Saved passenger count report to {filename}'.format(filename=filename))
+
+    return filename
--- test/test_pytest_basics.py
+++ test/test_pytest_basics.py
@@ -13,11 +13,21 @@ def test_basic():
     def add_2(x):
         return x + 2
 
-    # 
-    assert add_2(5) == 6
+    # use python standard assert to verify expectations
+    assert add_2(5) == 7
 
 
 # functions that aren't prefixed with test are ignored
 def i_am_ignored():
     pass
+
+
+# assert that an exception is raised
+def test_raise_exception():
+    def divide_by_zero():
+        5 / 0
+
+    with pytest.raises(ZeroDivisionError):
+        divide_by_zero()
+
 
